d855b55d7c3af4998b61185b8915fc6db4ec99f9
Aklesh Nonia <77534443+Aklesh150@users.noreply.github.com>
2025-08-03 01:06:55 +0530

    Add files via upload

---
+++ ReverseCountingNumber.java
@@ -0,0 +1,9 @@
+public class ReverseCountingNumber {
+    public static void main(String[] args) {
+        for(int i=100; i>=1; i--)
+        {
+            System.out.print(i);
+            System.out.print(" ,");
+        }
+    }
+}
