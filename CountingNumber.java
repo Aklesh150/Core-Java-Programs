c0389a5b0d1123b8652510483347105ae2c2b835
Aklesh Nonia <77534443+Aklesh150@users.noreply.github.com>
2025-08-03 00:59:49 +0530

    Add files via upload

---
+++ CountingNumber.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class CountingNumber {
+    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+        for(int i=1; i<=1000; i++)
+        {
+            System.out.print(i);
+            System.out.print(", ");
+        }
+    }
+}
