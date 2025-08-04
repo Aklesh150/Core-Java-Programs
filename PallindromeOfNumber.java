391ba3bf71d164cbedb763b1959ecfb7dc972ded
Aklesh Nonia <77534443+Aklesh150@users.noreply.github.com>
2025-08-04 23:01:32 +0530

    Add files via upload

---
+++ PallindromeOfNumber.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class PallindromeOfNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int temp = num;
+        int rem = 0;
+        int rev = 0;
+        while(temp !=0) {
+            rem = temp % 10;
+            temp = temp / 10;
+            rev = rev * 10 + rem;
+        }
+        if(num == rev) {
+            System.out.print("The number is pallindrome");
+        }
+            else{
+                System.out.println("The number is not pallindrome");
+            }
+
+
+    }
+}
