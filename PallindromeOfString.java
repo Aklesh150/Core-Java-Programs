0d3a05cb68e7a54f8a2030d9296c779665609f53
Aklesh Nonia <77534443+Aklesh150@users.noreply.github.com>
2025-08-04 23:33:00 +0530

    Add files via upload

---
+++ PallindromeOfString.java
@@ -0,0 +1,21 @@
+import java.sql.SQLOutput;
+import java.util.*;
+
+public class PallindromeOfString {
+    public static void main(String[] args) {
+        System.out.println("Enter the String");
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine().toLowerCase();
+        int d = str.length();
+        String rev = "";
+        for(int i = d-1; i>=0; i--){
+            rev = rev + str.charAt(i);
+        }
+        if(str.equals(rev)){
+            System.out.print("The string is a Pallindrome");
+        }
+        else{
+            System.out.print("The number is not a Pallindrome");
+        }
+    }
+}
