a48c13c3d47d4a913a49fe1e299c63a3b578055d
Aklesh Nonia <77534443+Aklesh150@users.noreply.github.com>
2025-08-05 22:28:45 +0530

    Update MultiplicationOfNumbers.java

--- MultiplicationOfNumbers.java
+++ MultiplicationOfNumbers.java
@@ -5,9 +5,10 @@ public class MultiplicationOfNumbers {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number");
         int num = sc.nextInt();
-        for(int i= 1; i<=num; i++){
+        for(int i= 1; i<=10; i++){
             System.out.println(num + "x" +i + "=" + (num*i));
 
         }
     }
 }
+
