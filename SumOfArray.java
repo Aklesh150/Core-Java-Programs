6d461414830c3679f3a376e4a8fd9a251980df0c
Aklesh Nonia <77534443+Aklesh150@users.noreply.github.com>
2025-08-07 21:21:46 +0530

    Add files via upload

---
+++ SumOfArray.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class SumOfArray{
+    public static void main(String[] args) {
+        int[] arr= {2,3,4,5,6,7,8,9};
+        int sum = 0;
+        for(int i = 0; i< arr.length; i++){
+            sum = sum + arr[i];
+        }
+        System.out.print(sum);
+    }
+}
