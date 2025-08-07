9077a8d2e7d112de1ce3ebe6c544753d36ac84d6
Aklesh Nonia <77534443+Aklesh150@users.noreply.github.com>
2025-08-07 13:14:17 +0530

    Update and rename PalindromeOfString.java to PalindromeOfString.java

--- PalindromeOfString.java
+++ PalindromeOfString.java
@@ -1,7 +1,7 @@
 import java.sql.SQLOutput;
 import java.util.*;
 
-public class PallindromeOfString {
+public class PalindromeOfString {
     public static void main(String[] args) {
         System.out.println("Enter the String");
         Scanner sc = new Scanner(System.in);
@@ -19,3 +19,4 @@ public class PallindromeOfString {
         }
     }
 }
+
